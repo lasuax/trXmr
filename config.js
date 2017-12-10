a7e6a469658ff41f0a0b562908dd5a5cb04af750
doğa gençer <doa@outlook.com>
2017-12-10 23:36:08 +0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+var api = "http://havuz.trxmr.com:8117";
+
+var coinUnits = 1000000000000;
+
+var poolHost = "havuz.trxmr.com";
+
+var irc = "irc.freenode.net/#monero-pools";
+
+var email = "yardim@trxmr.com";
+
+// var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "XMR-EUR", "XMR-GBP"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://chainradar.com/xmr/block/";
+
+var transactionExplorer = "http://chainradar.com/xmr/transaction/";
